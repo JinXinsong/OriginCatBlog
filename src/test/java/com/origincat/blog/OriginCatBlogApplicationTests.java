8abcb3f2615d05bdd7e1aa4ee257bad0aceb60a7
origincat <truealfajin@gmail.com>
2020-07-08 07:24:51 +0800

    start

---
+++ src/test/java/com/origincat/blog/OriginCatBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origincat.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OriginCatBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
