a0e906594a55abaf914170dff024cc4144544457
origincat <truealfajin@gmail.com>
2020-07-08 07:21:47 +0800

    start

---
+++ src/main/java/com/origincat/blog/OriginCatBlogApplication.java
@@ -0,0 +1,13 @@
+package com.origincat.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginCatBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OriginCatBlogApplication.class, args);
+    }
+
+}
